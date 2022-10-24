fb9952dff0308a4fd9f2b3a8294878fd29bdee9d
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-10-24 16:38:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ const bookName = document.querySelector('.bookName');
 const bookAuthor = document.querySelector('.bookAuthor');
 const form = document.querySelector('.addBook');
 const bookDisplay = document.querySelector('#bookList');
+var bookList = [];
 
-const bookList = [];
 
+//when the page loads
+//check for data in the local storage
+const displayBooks = JSON.parse(localStorage.getItem('bookData'));
+// bookList = savedData;
+console.log(bookList)
+
+if(bookList) {
+  populateUI()
+} else {
+  
+}
   
 function savetoLocalStorage() {
     // bookList = JSON.parse(localStorage.getItem('bookData'));
-    const bookData = {
-        name: bookName.value,
-        author: bookAuthor.value,
-    };
+      const name = bookName.value.trim();
+      const author =  bookAuthor.value.trim();
+    
+      // save to local storage
+      const bookData = {
+        name: name,
+        author: author
+      }
 
     bookList.push(bookData);
 
@@ -27,23 +42,23 @@ form.addEventListener('submit', (e) => {
 
 //   console.log(bookList);
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const bookListItems = JSON.parse(localStorage.getItem('bookData'));
-    if(bookListItems) {
-        bookListItems.forEach((bookData) => {
-            const bookDiv = document.createElement('div');
-            bookDiv.classList.add('book');
+//   document.addEventListener('DOMContentLoaded', () => {
+//     const bookListItems = JSON.parse(localStorage.getItem('bookData'));
+//     if(bookListItems) {
+//         bookListItems.forEach((bookData) => {
+//             const bookDiv = document.createElement('div');
+//             bookDiv.classList.add('book');
         
-            bookDiv.innerHTML = `<p>${bookData.name}</p>
-            <p>${bookData.author}</p>
-            <button class="removeBook">Remove</button>
-            <hr>
-            `;
-            bookDisplay.appendChild(bookDiv);
-            });
-    }
+//             bookDiv.innerHTML = `<p>${bookData.name}</p>
+//             <p>${bookData.author}</p>
+//             <button class="removeBook">Remove</button>
+//             <hr>
+//             `;
+//             bookDisplay.appendChild(bookDiv);
+//             });
+//     }
     
-  });
+//   });
 
 
 // for(let i = 0; i < bookData.length; i++){
