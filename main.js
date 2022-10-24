08aaf3c9fedc9388b205a278995bbc5224fa5c34
asharanjith <ashasvijayan@gmail.com>
2022-10-24 23:50:13 +0400

    Add linter and fix error

--- main.js
+++ main.js
@@ -2,38 +2,36 @@ const bookName = document.querySelector('.bookName');
 const bookAuthor = document.querySelector('.bookAuthor');
 const form = document.querySelector('.addBook');
 const bookDisplay = document.querySelector('.book');
-let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
+const bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 
 function displayBook() {
-    bookDisplay.innerHTML = bookList.map((book, i) => {
-        return `
+  bookDisplay.innerHTML = bookList.map((book, i) => `
             <li>
                 <p class="bookName">${book.name}</p>
                 <p class="bookAuthor">${book.author}</p> 
                 <button data-index=${i}>Remove</button>
                 <hr>
             </li>   
-        `;
-    }).join('');
+        `).join('');
 }
-  
+
 function addBook(e) {
-    e.preventDefault();
-    const name = bookName.value;
-    const author = bookAuthor.value;
-    const book = { name, author };
-    bookList.push(book);
-    displayBook();
-    localStorage.setItem('bookList', JSON.stringify(bookList));
-    this.reset();
+  e.preventDefault();
+  const name = bookName.value;
+  const author = bookAuthor.value;
+  const book = { name, author };
+  bookList.push(book);
+  displayBook();
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+  this.reset();
 }
 
 function deleteBook(e) {
-    if (!e.target.matches('button')) return;
-    const index = e.target.dataset.index;
-    bookList.splice(index, 1);
-    displayBook();
-    localStorage.setItem('bookList', JSON.stringify(bookList));
+  if (!e.target.matches('button')) return;
+  const { index } = e.target.dataset;
+  bookList.splice(index, 1);
+  displayBook();
+  localStorage.setItem('bookList', JSON.stringify(bookList));
 }
 
 form.addEventListener('submit', addBook);
