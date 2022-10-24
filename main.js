324bf56db3c0fe5444a7bfee359a8bcb5f671155
asharanjith <ashasvijayan@gmail.com>
2022-10-24 19:37:42 +0400

    Add Remove function

--- main.js
+++ main.js
@@ -2,23 +2,16 @@ const bookName = document.querySelector('.bookName');
 const bookAuthor = document.querySelector('.bookAuthor');
 const form = document.querySelector('.addBook');
 const bookDisplay = document.querySelector('#bookList');
-
-const bookList = [];
-
+let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
   
 function savetoLocalStorage() {
-    const bookList = JSON.parse(localStorage.getItem('bookData'));
-
-    const bookData = {
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+    let bookData = {
         name: bookName.value,
         author: bookAuthor.value,
     };
-
     bookList.push(bookData);
-
     localStorage.setItem('bookData', JSON.stringify(bookList));
-
-    console.log(bookData)
 }
 
 form.addEventListener('submit', (e) => {
@@ -27,31 +20,27 @@ form.addEventListener('submit', (e) => {
     form.reset();
   });
 
-//   console.log(bookList);
+  document.addEventListener('DOMContentLoaded', () => {
+    let bookListItems = JSON.parse(localStorage.getItem('bookData'));
+    if(bookListItems) {
+        bookListItems.forEach((bookData) => {
+            const bookDiv = document.createElement('div');
+            bookDiv.classList.add('book');
+        
+            bookDiv.innerHTML = `<p>${bookData.name}</p>
+            <p>${bookData.author}</p>
+            <button class="removeBook">Remove</button>
+            <hr>
+            `;
+            bookDisplay.appendChild(bookDiv);
+            });
+    }
+    
+  });
 
-//   document.addEventListener('DOMContentLoaded', () => {
-    // const bookList = JSON.parse(localStorage.getItem('bookData'));
-    // if (formDataInputs != null) {
-    //   nameElement.value = formDataInputs.name;
-    //   email.value = formDataInputs.email;
-    //   msgElement.value = formDataInputs.message;
-    // }
-//   });
-
-//   bookList.forEach((bookData) => {
-    // const bookDiv = document.createElement('div');
-    // bookDiv.classList.add('book');
-
-    // bookDiv.innerHTML = `<p>${bookData.name}</p>
-    // <p>${bookData.author}</p>
-    // <button class="removeBook">Remove</button>
-    // <hr>
-    // `;
-    // bookDisplay.appendChild(bookDiv);
-    // console.log(bookData)
-    // });
-
-// for(let i = 0; i < bookData.length; i++){
-//     console.log(bookList[i]);
-// }
-
+    bookDisplay.addEventListener('click', (e) => {
+        if(e.target.classList.contains('removeBook')) {
+            e.target.parentElement.remove();
+            // localStorage.removeItem('bookData');       
+        }
+    });
