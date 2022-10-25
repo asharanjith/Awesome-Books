d036cea94c79c9215349a5c45012acaf32ca2f76
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-10-25 11:45:24 +0100

    update

--- main.js
+++ main.js
@@ -2,57 +2,50 @@ const bookName = document.querySelector('.bookName');
 const bookAuthor = document.querySelector('.bookAuthor');
 const form = document.querySelector('.addBook');
 const bookDisplay = document.querySelector('#bookList');
+const bookList = [];
+const storedBooks = JSON.parse(localStorage.getItem('bookData'));
+const deleteBook = document.querySelectorAll('.removeBook')
 
+const displayBook = (bookItem) => {
+  const newBook = document.createElement('div');
+  newBook.classList.add('book');
+  newBook.innerHTML = `<h3>${bookItem.bookTitle}</h3>
+  <p>${bookItem.bookAuthor}</p>
+  <button class="removeBook" id="bookItem.bookTitle">Remove</button>
+<hr>`;
+bookDisplay.appendChild(newBook);
+};
 
-//when the page loads
-//check for data in the local storage
+if(storedBooks) {
+  for(let item of storedBooks) {
+    displayBook(item);
+  }
+};
 
-const populateUI = (bookData) => {
-  const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-
-    bookDiv.innerHTML = `<p>${bookData.name}</p>
-    <p>${bookData.author}</p>
-    <button class="removeBook">Remove</button>
-    <hr>
-    `;
-    bookDisplay.appendChild(bookDiv);
+function saveToLocalStorage() {
+  const book = {
+    bookTitle: bookName.value,
+    bookAuthor: bookAuthor.value
+  };
+  bookList.push(book);
+  displayBook(book);
+  localStorage.setItem('bookData', JSON.stringify(bookList));
 }
-
-
-const displayBooks = JSON.parse(localStorage.getItem('bookData'));
-
-if(displayBooks) {
-  displayBooks.forEach(bookItem => {
-    populateUI(bookItem)
-  });
-}
-
-function savetoLocalStorage() {
-    // bookList = JSON.parse(localStorage.getItem('bookData'));
-      const name = bookName.value.trim();
-      const author =  bookAuthor.value.trim();
-    
-      const bookList = [];
-      // save to local storage
-      const bookData = {
-        name: name,
-        author: author
-      }
-    bookList.push(bookData);
-    localStorage.setItem('bookData', JSON.stringify(bookList));
-
-    bookList.forEach(book => {
-      populateUI(book);
-    })
-}
-console.log(bookList)
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    savetoLocalStorage();
-    form.reset();
-  });
+  e.preventDefault();
+  saveToLocalStorage();
+  form.reset();
+})
 
+// const removeBook = () => {
 
+// };
+
+// deleteBook.forEach(btn => {
+//   btn.addEventListener('click', (e) => {
+//     const item = e.target.id;
+//     console.log(item);
+//   })
+// })
 
