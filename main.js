9b830414c67e79097fc7cfb52be3b13e65f3e4db
asharanjith <ashasvijayan@gmail.com>
2022-10-25 19:37:47 +0400

    Fix linters

--- main.js
+++ main.js
@@ -4,40 +4,36 @@ const form = document.querySelector('.addBook');
 const bookContainer = document.querySelector('.book-container');
 const bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 
-
 class Book {
-  constructor(name, author){
+  constructor(name, author) {
     this.name = name;
     this.author = author;
   }
 
-  displayBook (){
+  displayBook() {
     const newBook = document.createElement('div');
     newBook.classList.add('book');
     newBook.innerHTML = `<p>"${this.name}" by ${this.author}</p>
-    `;  
+    `;
     const deleteBook = document.createElement('button');
     deleteBook.classList.add('delete');
     deleteBook.textContent = 'Remove';
     deleteBook.addEventListener('click', () => {
       this.deleteBook(this.name);
     });
-    newBook.appendChild(deleteBook); 
+    newBook.appendChild(deleteBook);
     bookContainer.appendChild(newBook);
-
-  };
-
-  addBook() {    
-    bookList.push(this);    
-    localStorage.setItem('bookList', JSON.stringify(bookList));    
   }
 
-  deleteBook(name) {
-    const removeItem = bookList.filter((item) => {
-      return item.name !== name;
-    });
+  addBook() {
+    bookList.push(this);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+
+  deleteBook= (name) => {
+    const removeItem = bookList.filter((item) => item.name !== name);
     localStorage.setItem('bookList', JSON.stringify(removeItem));
-    location.reload();
+    window.location.reload();
   }
 }
 
@@ -45,11 +41,11 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const book = new Book(bookName.value, bookAuthor.value);
   book.addBook();
-  book.displayBook();  
+  book.displayBook();
   form.reset();
 });
 
-bookList.forEach(book => {
+bookList.forEach((book) => {
   const bookObj = new Book(book.name, book.author);
   bookObj.displayBook();
 });
